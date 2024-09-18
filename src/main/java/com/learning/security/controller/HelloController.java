bd53f0bec37baab6b6bd67ac4740af2f6196c2fa
Vijay Mohan <vijaymohan@ULTS.ULCCS>
2024-09-18 18:05:05 +0530

    Spring Security Codebase

---
+++ src/main/java/com/learning/security/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.learning.security.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to Homepage " + request.getSession().getId();
+    }
+
+}
---
+++ src/main/java/com/learning/security/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.learning.security.controller;
+
+import com.learning.security.entity.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1,"Vijay",25),
+            new Student(2,"Mohan",25)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public List<Student> addStudent(@RequestBody Student student){
+        students.add(student);
+        return students;
+    }
+
+}
